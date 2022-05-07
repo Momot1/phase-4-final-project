66d766c68bc3827b79f8cbcdcff1a9543b4deeb2
Mark Momot <markmomot03@gmail.com>
2022-05-07 12:13:45 -0400

    Created routes and components for structure

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,30 @@
 import Login from "./Login";
 import Signup from "./Signup";
 import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Cart from "./Cart";
+import Product from "./Product";
+import UserProfile from "./UserProfile";
 
 function App() {
-  const [user, setUser] = useState({})
 
-  useEffect(() => {
-    fetch("/me")
-    .then(resp => resp.json())
-    .then(setUser)
-  }, [])
-
-  function handleLogout(){
-    fetch("/logout", {method: "DELETE"}).then(resp => resp.json()).then(() => setUser({}))
-  }
 
   return (
-    <div>
-      <h1>Hello {user.name}</h1>
-      <Login/>
-      <Signup/>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Switch>
+        <Route exact path = "/"><Home/></Route>
+        <Route path="/login"><Login/></Route>
+        <Route path="/signup"><Signup/></Route>
+        <Route path="/cart"><Cart /></Route>
+        <Route path="/products/:id"><Product /></Route>
+        <Route path="/users/:username/about"><UserProfile /></Route>
+        <Route path="/">404 NOT FOUND</Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
 function Login(){
+
     const [formData, setFormData] = useState({
         username: "",
         password: ""
