1bfd64de55290fedb09d168b16696274433a76e8
Mark Momot <markmomot03@gmail.com>
2022-05-11 14:27:14 -0400

    Started setting up ability to change password

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,6 +1,33 @@
-function UserProfile(){
+import { useState } from "react"
+
+function UserProfile({user, setUser}){
+
+    const [formData, setFormData] = useState({})
+
+    function handleUserUpdateChange(e){
+        e.preventDefault()
+
+        fetch("/changepassword", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+    }
+
     return (
-        <></>
+        <div>
+            <form onSubmit={handleUserUpdateChange}>
+                <label>Old password: </label>
+                <input type="password" /><br/>
+                <label>New password: </label>
+                <input type="password" /><br/>  
+                <label>Re-Type password: </label>
+                <input type="password" /><br/> 
+                <button type="submit">Update</button>
+            </form>
+        </div>
     )
 }
 
