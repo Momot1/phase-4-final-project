dbafd5808e307ec461dcb382ef83f3e6918918d7
Mark Momot <markmomot03@gmail.com>
2022-05-18 16:29:38 -0400

    Updated styles

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
 import { addZeros } from "./functions.js"
 
-function Home({search, products, setProducts }){
+function Home({ products }){
     const productElements = products.map(product => <div key={product.id} className="border" style={{width: "40%", position: "relative", left: "30%"}}><Link style={{color: "#3a4187", padding: "5px"}} to={`/products/${product.id}`}>{product.name} - ${addZeros(product.price)}</Link> <p style={{padding: "5px"}}>{product.description}</p></div>)
 
     return (
-        <div style={{background: "#f2f2f2"}}>
+        <div >
             {productElements}
         </div>
     )
