65b95cb508a8ae50322688c29b4c985cb6ba4f38
Mark Momot <markmomot03@gmail.com>
2022-05-21 19:15:09 -0400

    Attempt to fix routing problems for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
-
-    resources :reviews, only: [:create, :update, :destroy]
-    resources :products
-    resources :cartproducts, only: [:create, :destroy]
-    resources :users, only: [:destroy]
-
-    # Login/logout routes
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
-
-    # Signup and crab user info routes
-    post "/signup", to: "users#create"
-    get "/me", to: "users#show"
-
-    patch "/changepassword", to: "users#update_password"
-
-    post "/addtocart", to: "users#add_to_cart"
-    delete "/removefromcart/:id", to: "users#remove_from_cart"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+      resources :reviews, only: [:create, :update, :destroy]
+      resources :products
+      resources :cartproducts, only: [:create, :destroy]
+      resources :users, only: [:destroy]
+
+      # Login/logout routes
+      post "/login", to: "sessions#create"
+      delete "/logout", to: "sessions#destroy"
+
+      # Signup and crab user info routes
+      post "/signup", to: "users#create"
+      get "/me", to: "users#show"
+
+      patch "/changepassword", to: "users#update_password"
+
+      post "/addtocart", to: "users#add_to_cart"
+      delete "/removefromcart/:id", to: "users#remove_from_cart"
+
+
+   
+
 end
