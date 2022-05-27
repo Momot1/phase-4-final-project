6b6249b909360539a0ed395caa94790a32747d33
Mark Momot <markmomot03@gmail.com>
2022-05-27 12:28:53 -0400

    Added image url and stock amount to be able to be updated for products

--- client/src/components/UpdateProduct.js
+++ client/src/components/UpdateProduct.js
@@ -4,7 +4,9 @@ function UpdateProduct({product, onUpdateItem}){
     const [formData, setFormData] = useState({
         name: product.name,
         price: product.price,
-        description: product.description
+        description: product.description,
+        image_url: product.image_url,
+        stock_amount: product.stock_amount
     })
 
     function updateForm(e, input){
@@ -25,8 +27,18 @@ function UpdateProduct({product, onUpdateItem}){
                 </div>
 
                 <div className="input-group mb-3">
+                    <span className="input-group-text form-font-size" aria-label="Stock Amount">Stock Amount</span>
+                    <input type="text" value={formData.stock_amount} className="form-control form-font-size" onChange={e => updateForm(e, "stock_amount")} />
+                </div>
+
+                <div className="input-group mb-3">
                     <span className="input-group-text form-font-size" aria-label="Description">Description</span>
                     <input type="text" value={formData.description} className="form-control form-font-size" onChange={e => updateForm(e, "description")} />
+                </div>
+
+                <div className="input-group mb-3">
+                    <span className="input-group-text form-font-size" aria-label="Image Url">Image Url</span>
+                    <input type="text" value={formData.image_url} className="form-control form-font-size" onChange={e => updateForm(e, "image_url")} />
                 </div>
 
                 <button type="submit" className="btn btn-light btn-lg">Update</button>
